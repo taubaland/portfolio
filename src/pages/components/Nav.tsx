46e3b8ba2bf6871717187ee621512ebab6533d92
Alex Toland <atoland@woolworths.com.au>
2023-10-01 22:47:09 +1100

    styled Nav, started on intro

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,26 +1,49 @@
 import avatar from '/avatar.png'
 import {motion} from 'framer-motion'
 import { useState } from "react"
+import {useMediaQuery} from "../../util/useMediaQuery"
+
+const navMotion = {
+    visible: {
+        opacity: 1,
+        transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.15,
+        },
+    },
+    hidden: {
+        opacity: 0,
+    },
+}
+const itemMotion = {
+    visible: {opacity: 1, x: 0},
+    hidden: { opacity: 0, x: -100},
+}
 
 export default function Nav() {
     const [toggled, setToggled] = useState(false)
+    const matches = useMediaQuery("(min-width: 1280px)")
+    // console.log(matches)
     return (
-        <nav className='relative mx-8 mb-24 flex jus items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32'>
+        <nav className='relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32'>
             <svg
                 className="absolute bottom-0 left-1/2 -translate-x-1/2"
-                width= "250"
+                width='w-screen'
                 height={4}
-                viewBox="0 0 250 4"
+                viewBox="0 0 w-screen 4"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
             >
-            
-            <path 
-            d="M2 2L428 2"
-            strokeWidth={2}
-            stroke="#282828"
-            strokeLinecap="round"
-            />
+
+            <line
+                x1="0vw"
+                x2="100vw"
+                y1="0"
+                y2="0" 
+                strokeWidth={3}
+                stroke="#282828"
+                strokeLinecap="round"
+                />    
 
             </svg>
 
@@ -34,11 +57,60 @@ export default function Nav() {
                 <a href="/">Alex Toland</a>
             </h1>
 
-            <div className="space-y-1">
-                <span className='block h-0.5 w-8 bg-black'></span>
-                <span className='block h-0.5 w-6 bg-black'></span>
-                <span className='block h-0.5 w-4 bg-black'></span>
+            {matches &&
+            <div className="flex gap-12">
+                <a href="/">Home</a>
+                <a href="/portfolio">Portfolio</a>
+                <a href="/cv">CV</a>  
+                <a href="/contact">Contact</a>
             </div>
+            }
+            
+            {!matches &&
+            <div onClick={() => setToggled((prevToggle) => !prevToggle)} 
+            className="space-y-1.5 cursor-pointer z-50">
+                <motion.span animate=
+                {{ 
+                    rotateZ: toggled ? 45 :0, 
+                    y: toggled ? 8 : 0
+                }} 
+                className='block h-0.5 w-8 bg-black'>
+                </motion.span>
+                <motion.span  animate=
+                    {{ 
+                        width: toggled ? 0 : 24
+                    }}
+                 className='block h-0.5 w-6 bg-black'>
+                </motion.span>
+                <motion.span animate=
+                    {{ 
+                        rotateZ: toggled ? -45 :0,
+                        y: toggled ? -8 : 0, 
+                        width: toggled ? 32 : 16
+                    }}  
+                    className='block h-0.5 w-4 bg-black'>
+                </motion.span>
+            </div>
+            }
+                
+            {toggled && !matches && (
+                <motion.div
+                    animate={{ opacity: 1, x:0}}
+                    initial={{ opacity: 0, x:25}}
+                    className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center"
+                    >
+                <motion.div variants={navMotion}
+                    animate="visible"
+                    initial="hidden"
+                    className="flex flex-col gap-24 text-lg">
+                    <motion.a variants={itemMotion} href="/">Home</motion.a>
+                    <motion.a variants={itemMotion} href="/portfolio">Portfolio</motion.a>
+                    <motion.a variants={itemMotion} href="/cv">CV</motion.a>
+                    <motion.a variants={itemMotion} href="/contact">Contact</motion.a>
+                </motion.div>
+                </motion.div>
+            )}
+
         </nav>
     )
 }
